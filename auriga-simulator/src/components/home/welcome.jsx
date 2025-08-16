4aba3dc978431e6fc704454ef41db7090424568b
PreciousM01 <feutsopm@carleton.edu>
2025-08-16 16:04:13 -0500

    Updating component files

---
+++ auriga-simulator/src/components/home/welcome.jsx
@@ -0,0 +1,13 @@
+export default function welcome() {
+
+    return(
+        <section className="text-center space-y-4">
+      <h1 className="text-4xl font-bold text-gray-900">
+        🌌 Welcome to <b>AuGa VS</b>
+      </h1>
+      <p className="text-lg text-gray-700">
+       The <b>AUriga GAlaxy Video Simulator</b> enables you to generate your customized video simulation to visualize the evolution of Galaxies from the Auriga project
+      </p>
+    </section>
+    );
+}
